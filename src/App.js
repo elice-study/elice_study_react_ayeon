4fe983f05da54d2f48dcf8ca8e70d19c02dd9a3e
joayeon <codinz.n@gmail.com>
2023-06-12 18:58:59 +0900

    :sparkles:  add test -ing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+    const [state, setState] = useState(true);
+
+    const onClickBtn = (e) => {};
+
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                    Learn React
-                </a>
-            </header>
+            <input id="input" />
+            <button onClick={onClickBtn}>입력</button>
+            <div></div>
         </div>
     );
 }
