905a2d71ce9f30de1b910b44dcd39b9efc982ea0
joayeon <codinz.n@gmail.com>
2023-06-19 18:13:31 +0900

    :sparkles:  add test

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import { useState } from 'react';
-import './App.css';
+import { useRef, useState } from 'react';
 
 function App() {
-    const [state, setState] = useState(true);
+    // 배열 생성
+    const arr = useRef([]);
+    const [state, setState] = useState(false);
 
-    const onClickBtn = (e) => {};
+    // 인풋 값 가져오기
+    const inputValue = useRef();
+
+    // 추가 버튼을 누르면 강제 렌더링 되어 배열에 추가
+    const onAddList = (e) => {
+        setState((prev) => !prev);
+        arr.current.push(inputValue.current.value);
+        e.preventDefault();
+    };
 
     return (
         <div className="App">
-            <input id="input" />
-            <button onClick={onClickBtn}>입력</button>
-            <div></div>
+            <input id="input" ref={inputValue} />
+            <button onClick={onAddList}>입력</button>
+
+            {arr.current.map((el) => (
+                <div>{el}</div>
+            ))}
         </div>
     );
 }
